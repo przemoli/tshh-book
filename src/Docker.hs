55e6d913b750981970328adddbe590a61dd7cedc
Przemysław Lib <przemyslaw.lib@gmail.com>
2022-06-01 21:47:46 +0200

    Using sockets to issue Docker commands

--- src/Docker.hs
+++ src/Docker.hs
@@ -1,6 +1,9 @@
 module Docker where
 
 import RIO
+import qualified Network.HTTP.Simple as HTTP
+import qualified Data.Aeson as Aeson
+import qualified Socket
 
 data CreateContainerOptions
     = CreateContainerOptions
@@ -14,7 +17,17 @@ imageToText :: Image -> Text
 imageToText (Image step) = step
 
 createContainer :: CreateContainerOptions -> IO ()
-createContainer options = undefined
+createContainer options = do
+    manager <- Socket.newManager "/var/run/docker.sock"
+    let body = Aeson.Null -- TODO figure out actual request body
+    let req = HTTP.defaultRequest
+            & HTTP.setRequestManager manager
+            & HTTP.setRequestPath "/v1.40/containers/create"
+            & HTTP.setRequestMethod "POST"
+            & HTTP.setRequestBodyJSON body
+    res <- HTTP.httpBS req
+    -- Dump the response to stdout to check what we' are getting back
+    traceShowIO res
 
 newtype ContainerExitCode = ContainerExitCode Int
     deriving (Eq, Show)
